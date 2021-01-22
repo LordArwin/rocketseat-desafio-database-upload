4167e921a7080de4fe7bcb213f4c3f24fa00ba2c
LordArwin <cleitonjunior2607@gmail.com>
2021-01-22 09:38:15 -0700

    Done!

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,47 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+    let categoryFind = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You not have Balance');
+    }
+
+    if (!categoryFind) {
+      categoryFind = categoryRepository.create({ title: category });
+      await categoryRepository.save(categoryFind);
+    }
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category: categoryFind,
+    });
+    await transactionRepository.save(transaction);
+    return transaction;
   }
 }
 
